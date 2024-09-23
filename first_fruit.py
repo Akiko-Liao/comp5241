433d900bf7f2698c714428b73cf00ab19b1578ba
Akiko-Liao <24044793G@connect.polyu.hk>
2024-09-23 09:29:20 +0000

    添加学生数据文件和水果处理功能

---
+++ first_fruit.py
@@ -0,0 +1,4 @@
+from hello import fruits
+
+
+first_fruit = fruits[0]
---
+++ hello.py
@@ -0,0 +1,14 @@
+
+from first_fruit import first_fruit
+
+
+print('hi')
+
+
+fruits = ['apple', 'banana', 'cherry']
+
+print(first_fruit) 
+
+fruits[1] = 'blueberry'
+print(fruits)
+
